31676eab66b38f9a9117ac740cf3d6f80babee2f
Henrique S.A <henriqueator@gmail.com>
2020-07-02 18:29:56 -0300

    Function returns the most common char in the string

---
+++ Algorithms/mostCommonChar.js
@@ -0,0 +1,26 @@
+// --- Directions
+// Given a string, return the character that is most
+// commonly used in the string.
+// --- Examples
+// maxChar("abcccccccd") === "c"
+// maxChar("apple 1231111") === "1"
+
+function maxChar(str) {
+  const map = {};
+  let max = 0;
+  let maxChar = '';
+
+  for (const char of str) {
+    map[char] = map[char] + 1 || 1;
+  }
+
+  for (const key in map) {
+    if (map[key] > max) {
+      max = map[key];
+      maxChar = key;
+    }
+  }
+  return maxChar;
+}
+
+console.log(maxChar('aaassd'));
